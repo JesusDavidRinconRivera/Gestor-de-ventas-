c97b6b36c13ba51ab1474e30d1acbf66c2171e8f
Jesus David Ricon Rivera <jesusdavidrinconrivera@gmail.com>
2022-03-24 20:52:04 -0500

    Agregamos 2 botones y 3 zonas de texto

--- Vista/VentanaPrincipal.java
+++ Vista/VentanaPrincipal.java
@@ -1,5 +1,3 @@
-
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -10,19 +8,20 @@ import javax.swing.JTextField;
 public class VentanaPrincipal extends JFrame {
 
     private JLabel texto;
-    private JTextField caja;
-    private JButton boton;
+    private JTextField caja1,caja2,caja3,caja4;
+    private JButton boton,Boton1,Boton2;
 
     public VentanaPrincipal() {
+
         configurarVentana();
         inicializarComponentes();
     }
 
     private void configurarVentana() {
-      int ancho = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;    //Coje la resolucion de la pantalla en alto
-      int alto = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;     //Coje la resolucion de la pantalla en ancho
-        this.setBounds((ancho / 2) - (this.getWidth() / 2), (alto / 2) - (this.getHeight() / 2), 500, 500); // Hace que siempre este a la misma resolucion
+      int ancho = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
+      int alto = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
         this.setTitle("Esta Es Una Ventana");
+        this.setBounds((ancho / 2) - (this.getWidth() / 2), (alto / 2) - (this.getHeight() / 2), 1600, 1000);
         this.setLocationRelativeTo(null);
         this.setLayout(null);
         this.setResizable(false);
@@ -30,18 +29,42 @@ public class VentanaPrincipal extends JFrame {
     }
 
     private void inicializarComponentes() {
+        // creamos los componentes
         texto = new JLabel();
-        caja = new JTextField();
+        caja1 = new JTextField();
+        caja2 = new JTextField();
+        caja3 = new JTextField();
+        caja4 = new JTextField();
         boton = new JButton();
+        // configuramos los componentes
         texto.setText("Inserte Nombre");
         texto.setBounds(50, 50, 100, 25);
-        caja.setBounds(150, 50, 100, 25);
+        caja1.setBounds(150,50, 100, 25);
+        caja2.setBounds(150,250, 100, 25);
+        caja3.setBounds(150,350, 100, 25);
+        caja4.setBounds(150, 450, 100, 25);
+
         boton.setText("Mostrar Mensaje");
         boton.setBounds(50, 100, 200, 30);
+
+
         this.add(texto);
-        this.add(caja);
+        this.add(caja1);
+        this.add(caja2);
+        this.add(caja3);
+        this.add(caja4);
         this.add(boton);
+
+        //Botón de Tal
+      Boton1 = new JButton();
+      Boton1.setText("Boton 2");
+      Boton1.setBounds(150, 150, 200, 30);
+      this.add(Boton1);
+
+      //botón de otro tal
+      Boton2 = new JButton();
+      Boton2.setText("Boton 3");
+      Boton2.setBounds(200, 200, 200, 30);
+      this.add(Boton2);
     }
-
-
 }
