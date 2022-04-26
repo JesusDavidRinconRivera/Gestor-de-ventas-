50ed12389ca288e4848955662c6180cd9a42b51d
JRarc <joseruben201@gmai.com>
2022-04-26 18:29:08 -0500

    Base de datos conectada

---
+++ Vista/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    conect con = new conect();
+    VentanaPrincipal Ventana1 = new VentanaPrincipal();
+    Ventana1.setVisible(true);
+
+  }
+}
---
+++ Vista/conect.java
@@ -0,0 +1,15 @@
+import java.sql.*;
+
+public class conect {
+    Connection con;
+
+    public conect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tabla", "root", "");
+            System.out.println("conectó esa madre");
+        } catch (Exception e) {
+            System.err.println("Error" + e);
+        }
+    }
+}
